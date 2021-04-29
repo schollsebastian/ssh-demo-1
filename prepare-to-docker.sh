c4e62b2c1727f7a15602995be60df2437b1b1916
StarkaLukas <l.starka@students.htl-leonding.ac.at>
2021-04-29 10:49:55 +0200

    add prepare-to-docker.sh

---
+++ prepare-to-docker.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+mv target/ssh-demo-*-runner.jar src/main/docker/demo.jar
--- src/main/docker/scripts/run.sh
+++ src/main/docker/scripts/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "starting quarkus $RUNNER..."
-java -Dquarkus.profile=docker -jar ssh-demo-1.0.0-SNAPSHOT-runner.jar
+java -Dquarkus.profile=docker -jar demo.jar
