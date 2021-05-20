f1e266e261fce99b639ff02ee3b922d9c5d339b1
StarkaLukas <l.starka@students.htl-leonding.ac.at>
2021-05-20 11:46:42 +0200

    create package and build docker image

--- prepare-to-docker.sh
+++ prepare-to-docker.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+rm ./src/main/docker/demo.jar
+mvn -B clean package
 mv target/ssh-demo-*-runner.jar src/main/docker/demo.jar
+
+docker build -t registry.cloud.htl-leonding.ac.at/l.starka/demo ./src/main/docker
+docker image ls
