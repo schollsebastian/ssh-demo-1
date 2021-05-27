3e73aca3be582357d68a945f5faa56abcfe8b72c
schollsebastian <scholl.sebastian@gmx.at>
2021-05-27 11:19:05 +0200

    Change username

--- prepare-to-docker.sh
+++ prepare-to-docker.sh
@@ -4,5 +4,5 @@ rm ./src/main/docker/demo.jar
 mvn -B clean package
 mv target/ssh-demo-*-runner.jar src/main/docker/demo.jar
 
-docker build -t registry.cloud.htl-leonding.ac.at/l.starka/demo ./src/main/docker
+docker build -t registry.cloud.htl-leonding.ac.at/s.scholl/demo ./src/main/docker
 docker image ls
